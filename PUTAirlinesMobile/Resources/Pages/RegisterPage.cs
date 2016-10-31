c07b322efdc435068785befb9ad64df5f3813c64
Drygi <Drygi@DESKTOP-ED366E5>
2016-10-31 20:34:21 +0100

    version 1.6

--- PUTAirlinesMobile/Resources/Pages/RegisterPage.cs
+++ PUTAirlinesMobile/Resources/Pages/RegisterPage.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
@@ -22,13 +21,14 @@ namespace PUTAirlinesMobile
         ProgressBar registerBar;
         EditText login, password1, password2, name, lastName, passsportNumber, nationality, city, street, postCode;
         MySqlConnection connection;
+        bool goodRegister;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.RegisterPage);
             controlsInit();
             connection = Helper.MySQLHelper.getConnection("Server=mysql8.mydevil.net;Port=3306;Database=m1245_paragon;User=m1245_paragon;Password=KsiVnj8HQz32VxT8eNPd");
-            
+            goodRegister = true;
         }
 
         void controlsInit()
@@ -40,6 +40,11 @@ namespace PUTAirlinesMobile
             registerBar = FindViewById<ProgressBar>(Resource.Id.registerProgressBar);
             registerBar.Visibility = ViewStates.Invisible;
 
+
+        }
+
+        void variabelsFromAXAML()
+        {
             login = FindViewById<EditText>(Resource.Id.registerLogin);
             password1 = FindViewById<EditText>(Resource.Id.registerPassword);
             password2 = FindViewById<EditText>(Resource.Id.registerPassword2);
@@ -50,24 +55,42 @@ namespace PUTAirlinesMobile
             city = FindViewById<EditText>(Resource.Id.registerCity);
             street = FindViewById<EditText>(Resource.Id.registerStreet);
             postCode = FindViewById<EditText>(Resource.Id.registerPostCode);
-
-
         }
-
-
         private void Register_Click(object sender, EventArgs e)
         {
             registerBar.Visibility = ViewStates.Visible;
 
+            variabelsFromAXAML();
             bool loginExist = Helper.MySQLHelper.findLogin(this.login.Text, connection);
 
-            if (loginExist) setAlert("Podany login juz istnieje");
+            if (loginExist)
+            {
+                goodRegister = false;
+                FindViewById<EditText>(Resource.Id.registerLogin).Text = "";
+                FindViewById<EditText>(Resource.Id.registerLogin).Hint = "Podany login ju¿ istnieje!";
+                FindViewById<EditText>(Resource.Id.registerLogin).SetHintTextColor(Android.Graphics.Color.Red);
+                registerBar.Visibility = ViewStates.Invisible;
+                FindViewById<ScrollView>(Resource.Id.scrollViewRegister).SmoothScrollTo(0, 0);
+            }
 
-            if (password1 != password2) setAlert("Podane has³a musz¹ byæ identyczne");
+            if (password1.Text != password2.Text)
+            {
+                goodRegister = false;
+                FindViewById<EditText>(Resource.Id.registerPassword).Text = "";
+                FindViewById<EditText>(Resource.Id.registerPassword2).Text = "";
+                FindViewById<EditText>(Resource.Id.registerPassword).Hint = "Podane has³a musz¹ byæ identyczne";
+                FindViewById<EditText>(Resource.Id.registerPassword).SetHintTextColor(Android.Graphics.Color.Red);
+                FindViewById<EditText>(Resource.Id.registerPassword2).Hint = "Podane has³a musz¹ byæ identyczne";
+                FindViewById<EditText>(Resource.Id.registerPassword2).SetHintTextColor(Android.Graphics.Color.Red);
+                FindViewById<ScrollView>(Resource.Id.scrollViewRegister).SmoothScrollTo(0, 0);
+                registerBar.Visibility = ViewStates.Invisible;
+            }
 
-           
+            if(goodRegister)
+            {
+                setAlert("Zarejestrowano pomyœlnie");
+            }
         }
-
         private void setAlert(string message)
         {
             Android.App.AlertDialog.Builder alert = new Android.App.AlertDialog.Builder(this);
